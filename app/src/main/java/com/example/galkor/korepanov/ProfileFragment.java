db7873e27f9a5b9239c8240cd8a74cfe1b858a48
wildkodokushi <bronnikovvd@gmail.com>
2026-04-23 18:04:17 +0400

    init commit

---
+++ app/src/main/java/com/example/galkor/korepanov/ProfileFragment.java
@@ -0,0 +1,33 @@
+package com.example.galkor.korepanov;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import com.example.galkor.R;
+
+public class ProfileFragment extends Fragment {
+    private TextView emailText;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_profile, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        emailText = view.findViewById(R.id.emailText);
+
+        if (emailText != null) {
+            emailText.setText("Профиль пользователя");
+        }
+    }
+}
